439ef38b5ec13b7f134f460cf9a3ee33243ed158
Tejas Khedkar <tejaskhedkar321@gmail.com>
2022-01-23 11:34:35 +0530

    jsx rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+
+//stateless function component
+//always return JSX
+//div/section/article of fragment
+//className instead of class
+//close every element
+//formating
+
 function Greeting(){
-    return <h4>this is tejas and this is my
-         first component</h4>;
-}
+    return( 
+    <div>
+        <h4>Hello World</h4>
+        <ul>
+            <li>
+                <a herf='#'>hello world</a>
+            </li>
+            <img src='' alt=''/>
+        </ul>
+    </div>
+    );
+    }
+
+// const Greeting= () =>{
+//     return React.createElement
+//     ('div',{},
+//     React.createElement('h1',{},'hello world'))
+// }
 
 ReactDOM.render(<Greeting/>,document.getElementById('root'))
