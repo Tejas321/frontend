9c1343c6bc891832edea59aa51c4b4e2f9480be3
Tejas Khedkar <tejaskhedkar321@gmail.com>
2022-01-24 21:59:24 +0530

    deployment

--- src/books.js
+++ src/books.js
@@ -15,4 +15,20 @@
     img:"https://images-na.ssl-images-amazon.com/images/I/71rdsaOMvVL._UX300__PJku-headphones-v1,TopRight,0,-50_AC_UL254_SR254,254_.jpg" ,
     title:"Good Thoughts",
     auther:"Atharav"
-}];
+},
+{id:4,
+    img:"https://images-eu.ssl-images-amazon.com/images/I/31gZ85pMqrL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg" ,
+    title:"Good Vibes",
+    auther:"Vex Kings"
+},
+{id:5,
+    img:"https://images-eu.ssl-images-amazon.com/images/I/51rVr4utFRL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg" ,
+    title:"Good Vibes",
+    auther:"Shwetabh Gangwar "
+},
+{id:6,
+    img:"https://images-eu.ssl-images-amazon.com/images/I/51kDHcBZkOL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg" ,
+    title:"Master Guide SSC CGL Combined Graduate Level Pre Exam Tier 1 2022",
+    auther:"Arihant Experts"
+}
+];
